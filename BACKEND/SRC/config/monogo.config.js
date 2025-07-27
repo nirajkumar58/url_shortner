74ba273bc686a8dea20776b5a2935490d4fedcf6
nirajkumar58 <nirajkumar934792@gmail.com>
2025-07-27 16:14:57 +0530

    Updated backend logic, controllers, services, and utils

--- BACKEND/SRC/config/monogo.config.js
+++ BACKEND/SRC/config/monogo.config.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
+console.log(process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB connected successfully");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
